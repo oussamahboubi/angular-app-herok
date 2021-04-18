1bbc080c4cc3b8236512a2dd897ee049367cdcbb
oussamahboubi <elmahboubi.ensias@gmail.com>
2021-04-18 14:00:08 +0000

    new commit

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/angular-app-heroku/'));
 
 app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname + '/dist/index.html')),
+    res.sendFile(path.join(__dirname + '/dist/angular-app-heroku/index.html')),
 );
 
 // Start the app by listening on the default Heroku port
