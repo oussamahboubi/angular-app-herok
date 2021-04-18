59a6e507058530e7a17edf0601a01d06345a4356
oussamahboubi <elmahboubi.ensias@gmail.com>
2021-04-18 13:18:11 +0000

    new commit

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-app-herok'));
+app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-app-herok/'}),
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname + '/dist/index.html')),
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 8080);
